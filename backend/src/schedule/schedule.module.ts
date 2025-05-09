76d73b25c1c350ce80d1af3fb2d0bacdfb6524a4
BennettPompi <bennettp2002@gmail.com>
2025-05-09 16:34:02 -0400

    deplaning

--- backend/src/schedule/schedule.module.ts
+++ backend/src/schedule/schedule.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ScheduleService } from './schedule.service';
-import { ScheduleController } from './schedule.controller';
-import { ScheduleEntity } from './schedule.entity';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ScheduleService } from "./schedule.service";
+import { ScheduleController } from "./schedule.controller";
+import { ScheduleEntity } from "./schedule.entity";
+import { ShiftModule } from "../shift/shift.module";
+import { NurseModule } from "@src/nurse/nurse.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ScheduleEntity])],
-  exports: [TypeOrmModule],
-  providers: [ScheduleService],
-  controllers: [ScheduleController],
+    imports: [
+        TypeOrmModule.forFeature([ScheduleEntity]),
+        ShiftModule,
+        NurseModule,
+    ],
+    exports: [TypeOrmModule],
+    providers: [ScheduleService],
+    controllers: [ScheduleController],
 })
 export class ScheduleModule {}
--- backend/src/schedule/schedule.service.ts
+++ backend/src/schedule/schedule.service.ts
@@ -2,15 +2,35 @@ import { Injectable, NotImplementedException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { ScheduleEntity } from "./schedule.entity";
+import { ShiftService } from "@src/shift/shift.service";
+import { NurseService } from "@src/nurse/nurse.service";
+import { ShiftEntity, ShiftRequirements } from "@src/shift/shift.entity";
+import { ShiftPreference } from "@m7-scheduler/dtos";
+import { NurseEntity } from "@src/nurse/nurse.entity";
 
+type SimpleNurse = {
+    id: number;
+    preferences: ShiftPreference[];
+    assignedCount: number;
+};
+type ScheduleShift = ShiftRequirements & {
+    assignedNurses: SimpleNurse[];
+    availableNurses: SimpleNurse[];
+};
 @Injectable()
 export class ScheduleService {
     constructor(
         @InjectRepository(ScheduleEntity)
-        private readonly scheduleRepository: Repository<ScheduleEntity>
+        private readonly scheduleRepository: Repository<ScheduleEntity>,
+        private readonly shiftService: ShiftService,
+        private readonly nurseService: NurseService
     ) {}
+    createShifts(shiftsRaw: ScheduleShift): ShiftEntity[] {
+        const shifts: ShiftEntity[] = [];
 
-    async generateSchedule(startDate: Date, endDate: Date): Promise<any> {
+        return shifts;
+    }
+    async generateSchedule(): Promise<ScheduleEntity> {
         /* 
             1. Go through nurses/prefs, create list of nurses available for each shift
             2. Maintain count of nurses assigned to each shift, sort each shift pool by count (asc)
@@ -20,7 +40,69 @@ export class ScheduleService {
                 If they were assigned to the previous 2 shifts, exclude them no matter what
             FINALLY: Iterate through shift assignments, create shift objects for each of them
         */
-        throw new NotImplementedException();
+        const schedule: ScheduleShift[] = (
+            await this.shiftService.getShiftRequirements()
+        ).map((shift) => {
+            return {
+                ...shift,
+                assignedNurses: [],
+                availableNurses: [],
+            };
+        });
+        const nurses: SimpleNurse[] = (await this.nurseService.getNurses()).map(
+            (nurse) => {
+                return {
+                    id: nurse.id,
+                    preferences: nurse.preferences,
+                    assignedCount: 0,
+                };
+            }
+        );
+
+        for (const nurse of nurses) {
+            const preferences = nurse.preferences;
+            for (let i = 0; i < preferences.length; i += 2) {
+                const dayPref = preferences[i];
+                if (dayPref.dayShift == true)
+                    schedule[i].availableNurses.push(nurse);
+                if (dayPref.nightShift == true)
+                    schedule[i + 1].availableNurses.push(nurse);
+            }
+        }
+        for (let i = 0; i < schedule.length; i++) {
+            const shift = schedule[i];
+            const available = shift.availableNurses;
+            //prioritize nurses with fewest assignments
+            available.sort(
+                (nurseA, nurseB) => nurseA.assignedCount - nurseB.assignedCount
+            );
+
+            const wouldBeDouble = [];
+            for (const nurse of available) {
+                if (shift.assignedNurses.length == shift.nursesRequired) break;
+
+                if (i > 0 && schedule[i - 1].assignedNurses.includes(nurse)) {
+                    // if this would be a triple shift for the nurse, we don't want to assign them at all as I assume this is illegal
+                    if (i > 1 && schedule[i - 2].assignedNurses.includes(nurse))
+                        continue;
+                    // if only a double, put them into reserve pool in case we need more later.
+                    wouldBeDouble.push(nurse);
+                    continue;
+                }
+                shift.assignedNurses.push(nurse);
+            }
+            // if we need more, pull double shift nurses until we have enough or run out
+            if (shift.assignedNurses.length < shift.nursesRequired) {
+                for (const nurse of wouldBeDouble) {
+                    if (shift.assignedNurses.length == shift.nursesRequired)
+                        break;
+                    shift.assignedNurses.push(nurse);
+                }
+            }
+        }
+        const dbSchedule = new ScheduleEntity();
+
+        return dbSchedule;
     }
 
     async getSchedules(): Promise<ScheduleEntity[]> {
